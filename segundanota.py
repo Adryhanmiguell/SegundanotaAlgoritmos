961ecb75f41f6654b75bac1065eea34c8f2aba64
Adryhanmiguell <migueladryhan202@gmail.com>
2025-10-28 20:16:52 -0300

    Add files via upload

--- segundanota.py
+++ segundanota.py
@@ -1,86 +1,101 @@
-
-usuarios = []
+usuario = []
+admin = []
 produtos = []
-
-print('\n\n')
-print('-'*50)
-print('MENU PRINCIPAL')
-print('-'*50)
 
 while True:
     while True:
-        print("1 - Cadastrar usuário\n" "2 - Login\n" "3 - Sair do sistema \n")
-        opcao = input("\nEscolha uma opção: ")
-        if opcao == '1':
-            nome = input("\nDigite seu nome: ")
-            email = input("Digite seu email: ")
-            if '@gmail.com' not in email:
+        print("")
+        print('-'*50)
+        print('- - - - - - - - - MENU PRINCIPAL - - - - - - - - -')
+        print('-'*50)
+        print(
+            "1 - Cadastrar usuário \n"
+            "2 - Cadastrar administrador \n"
+            "3 - Login \n" 
+            "4 - Sair \n"
+            )
+        opcao = int(input("\nOpção: "))
+        
+        if opcao == 1:
+            nome = input("\nNome: ")
+            email = input("Email: ")
+
+            while '@gmail.com' not in email:
+                print('\nEmail invalido !')
+                email = input("Digite novamente: ")
+                break
+
+            senha = input("Senha: ")
+            while len(senha) < 4:
+                print("Senha com menos de 4 caracteres, digite novamente")
+                senha = input("Digite sua senha novamente: ")
+                break
+
+            usuario.append(nome)
+            usuario.append(email)
+            usuario.append(senha)   
+            print("\nUsuario cadastradado com sucesso.") 
+            break
+
+        elif opcao == 2:
+            nome = input("\nNome: ")
+            email = input("Email: ")
+
+            while '@gmail.com' not in email:
+                print('\nEmail invalido !')
+                email = input("Digite novamente: ")
+                break
+
+            senha = input("Senha: ")
+            while len(senha) < 4:
+                print("Senha com menos de 4 caracteres, digite novamente")
+                senha = input("Digite sua senha novamente: ")
+                break
+
+            admin.append(nome)
+            admin.append(email)
+            admin.append(senha)   
+            print("\nAdministrador cadastradado com sucesso.") 
+
+        
+        elif opcao == 3:
+            email = input("\nEmail: ")
+            senha = input("Senha: ")
+            
+            if senha and email in usuario:
                 while True:
-                    print('\nErro, email invalido')
-                    email = input("Digite seu email novamente: ")
-                    if '@gmail.com' in email:
-                        break
+                    print('')
+                    print('-'*50)
+                    print('- - - - - - - - - MENU USUARIO - - - - - - - - -')
+                    print('-'*50)
+                    print(
+                    "1 - Comprar produtos e agendar serviços\n"
+                    "2 - Logout\n"
+                    )
+                    opcao = input("Opção: ")
+                    break
+
+
+
+            elif senha and email in admin:
+                while True:
+                    print('')
+                    print('-'*50)
+                    print('- - - - - - - - - MENU ADMIN - - - - - - - - -')
+                    print('-'*50)
+                    print(
+                        "1 - Gerenciar os produtos e serviços\n"
+                        "2 - Cadastrar produto \n"
+                        "3 - Cadastrar serviço\n"
+                        "4 - Buscar produto/serviço \n"
+                        "5 - Atualizar dados do produto/serviço\n"
+                        "6 - Remover produto/serviço \n"
+                        "7 - Logout\n"
+                    )
+                    opcao = input("Opção: ")
+
             else:
-                usuarios.append(nome,email)
-                print(f'usuario logado {usuarios}')
+                print("Credenciais não encontradas !") 
 
-            senha = input("Digite sua senha: ")
-            if len(senha) < 4:
-                while True:
-                    print("Senha com menos de 4 caracteres, digite novamente")
-                    senha = input("Digite sua senha novamente: ")
-                    if len(senha) >= 4:
-                        break
-        
-        elif opcao == '2':
-            nome = input("\nDigite seu nome: ")
-            email = input("Digite seu email: ")
-            if '@gmail.com' not in email:
-                while True:
-                    print('\nErro, email invalido')
-                    email = input("Digite seu email novamente: ")
-                    if '@gmail.com' in email:
-                        break
 
-            senha = input("Digite sua senha: ")
-            if len(senha) < 4:
-                while True:
-                    print("Senha com menos de 4 caracteres, digite novamente")
-                    senha = input("Digite sua senha novamente: ")
-                    if len(senha) >= 4:
-                        break
-
-        while True:
-            print('\n\n')
-            print('-'*50)
-            print('MENU ADMIN')
-            print('-'*50)
-
-            print("1 - Cadastrar usuário\n"
-                "2 - Gerenciar os produtos e serviços\n"
-                "3 - Cadastrar produto \n"
-                "4 - Cadastrar serviço\n"
-                "5 - Buscar produto/serviço \n"
-                "6 - Atualizar dados do produto/serviço\n"
-                "7 - Remover produto/serviço \n"
-                "8 - Logout\n")
-            
-
-        #elif opcao == '3':
-            #verificacao = False
-            #email = input("Digite seu email: ")
-            #senha = input("Digite sua senha: ")
-            #print()
-            
-            #usuario_logado = None
-            #for usuario in usuarios:
-                #if email == usuario["email"] and senha == usuario["senha"]:
-                    #print(f"Nome: {usuario['nome']}")
-                    #print("-" * 50)
-                    #verificacao = True
-                    #usuario_logado = usuario
-                    #break
-        #elif opcao == '0':
-        
-        
-        print('Cadrastro feito com sucesso! ')
+# se os dados estiver igual vai logar no primeiro if, tem que arrumar um jeito de nao poder ter as infos iguais
